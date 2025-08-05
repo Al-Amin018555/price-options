807320dd33418183121302438e92476bd69c8fbf
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2025-08-05 22:58:51 +0600

    added react loading spinner while bar chart data load

--- src/Components/Phones/Phones.jsx
+++ src/Components/Phones/Phones.jsx
@@ -1,9 +1,11 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import {useState } from "react";
 import { BarChart, Bar, Rectangle, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { Audio,Grid } from 'react-loader-spinner'
 
 const Phones = () => {
     const [phones, setPhones] = useState([]);
+    const [loading, setLoading] = useState(true);
     // useEffect(()=>{
     //     fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
     //     .then(res => res.json())
@@ -22,12 +24,35 @@ const Phones = () => {
                 }
                 return obj;
             })
-            
+
             setPhones(phonesWithFakeData);
+            setLoading(false);
         })
 
     return (
         <div>
+            {loading && <div>
+
+                <Audio
+                    height="80"
+                    width="80"
+                    radius="9"
+                    color="green"
+                    ariaLabel="three-dots-loading"
+                    wrapperStyle
+                    wrapperClass
+                />
+                <Grid
+                    visible={true}
+                    height="80"
+                    width="80"
+                    color="#4fa94d"
+                    ariaLabel="grid-loading"
+                    radius="12.5"
+                    wrapperStyle={{}}
+                    wrapperClass="grid-wrapper"
+                />
+            </div>}
             <p className="text-7xl mt-10">Phones: {phones.length}</p>
             <BarChart className="mt-6" width={1200} height={400} data={phones}>
                 <Bar dataKey='price' fill="#8884d8" />
